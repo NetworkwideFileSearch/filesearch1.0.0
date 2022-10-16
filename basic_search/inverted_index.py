c4c7e0d5d1b16c665c53fd2c5d70e8efe7f08133
mayuresh714 <mayureshkhanaj@gmail.com>
2022-10-17 00:54:08 +0530

    stopping inverted index work focusing on search with help of pandas inbuilt functions

---
+++ basic_search/inverted_index.py
@@ -0,0 +1,46 @@
+ 
+
+
+class Inverted_Index:
+    
+    def __init__(self,token_dict = {}):
+        self.token_dict = token_dict
+
+    def tokenize_file_name(self,filename):
+        token_list = {}
+        word = ""
+        for i in filename:
+            if i.isalnum():
+                word += i.lower()
+            else:
+                if word in token_list:
+                    token_list[word] += 1
+                else:
+                    token_list[word] = 1
+                word = ""
+        if word in token_list:
+            token_list[word] += 1
+        else:
+            token_list[word] = 1
+        return token_list  
+
+    def add_file_to_token_dict(self,lis,doc_id):
+        for ind,token in enumerate(lis):
+            token = token.lower()
+            if token in self.token_dict:
+                self.token_dict[token].append(doc_id)
+            else:
+                self.token_dict[token] = [doc_id]
+
+    def inverting_file(self,filename,doc_id):
+        token_lis = self.tokenize_file_name(filename)
+        self.add_file_to_token_dict(token_lis,doc_id)
+
+
+
+if __name__ == "__main__":
+    obj = Inverted_Index()
+    obj.inverting_file(filename="Criminal.Justice.Episode.6.720p.moviebaba.mkv",doc_id=10)
+    print(obj.token_dict)
+
+    
---
+++ basic_search/search.py
@@ -0,0 +1,116 @@
+ 
+import os
+import time
+import pandas as pd
+ 
+
+class Filemanager:
+
+    def __init__(self,path = "C:/users/hp/desktop/" ):  ## each distinct object will require path of folder whose files u wanna fetch
+        self.path = path
+        self.lis  = []
+        self.db   = None
+        self.ignore_file_dict = {'.git':0,  ## add file types that you dont want to search
+                                "__pycache__":0}  
+
+
+    def get_list_of_files(self,folder_path):
+        try:
+            return os.listdir(folder_path)
+        except:
+            return -1
+
+
+
+    def DFS_method(self,path):  ## use DFS recursion as folder file structure is basically a tree structure
+        
+        lis_of_dirs = self.get_list_of_files(path)  ## get list of files in directory
+        if lis_of_dirs != -1:
+            for file in lis_of_dirs:
+                if file in self.ignore_file_dict: # check if file type is to be ignored or not
+                    continue
+
+                fpath = path + "/" + file
+
+                # if  not self.is_file_accessable(fpath): ## check if file is accessible to mode like read , write
+                #     continue
+                
+                if os.path.isdir(fpath):  ## check if path is folder
+                    self.get_folder_metadata(fpath,foldername=file)
+                    self.DFS_method(fpath)
+                
+                elif os.path.isfile(fpath): ## check if path is file 
+                    self.get_file_metadata(fpath,filename=file)
+
+
+
+    def is_file_accessable(self,file_path):
+        if os.path.exists(file_path):
+            # path exists
+            if os.path.isfile(file_path): # is it a file or a dir?
+                # also works when file is a link and the target is writable
+                return os.access(file_path, os.R_OK)
+            else:
+                return False # path is a dir, so cannot write as a file
+        # target does not exist, check perms on parent dir
+        pdir = os.path.dirname(file_path)
+        if not pdir: pdir = '.'
+        # target is creatable if parent dir is writable
+        return os.access(pdir, os.W_OK)
+
+
+
+    def get_folder_metadata(self, folderpath,foldername ):  ## it appends all metadata of a single file in lis of dictiories named as self.dic
+        try:
+            metadata =   {
+                "file_name"    : foldername, 
+                "file_path"    : folderpath,
+                'Access_time'  : time.ctime(os.path.getatime(folderpath)),
+                'Modified_time': time.ctime(os.path.getmtime(folderpath)),
+                'Change_time'  : time.ctime(os.path.getctime(folderpath)),
+                'Size'         : os.path.getsize(folderpath),
+                'type'         : "folder"
+            }
+            self.lis.append(metadata)
+        except:
+            pass
+
+
+    def get_file_metadata(self, filepath,filename ):  ## it appends all metadata of a single file in lis of dictiories named as self.dic
+        try:
+            metadata =   {
+                "file_name"    : filename ,#.split(".")[0]   if "." in filename    else  filename,
+                "file_path"    : filepath,
+                'Access_time'  : time.ctime(os.path.getatime(filepath)),
+                'Modified_time': time.ctime(os.path.getmtime(filepath)),
+                'Change_time'  : time.ctime(os.path.getctime(filepath)),
+                'Size'         : os.path.getsize(filepath) ,
+                'type'         : filepath.split(".")[-1]   if "." in filepath    else  filepath
+            }
+            self.lis.append(metadata)
+        except:
+            # print(f"{filepath} is not accessible to read..")
+            pass
+            
+
+    def get_df(self):   ## this method will convert lis of dictionaries (metadata ) into dataframe
+        return pd.DataFrame(self.lis,columns = ["file_name","file_path",'Access_time','Modified_time','Change_time','Size','type' ])
+        
+
+    def process(self):  
+        self.DFS_method(self.path)
+        self.db = self.get_df()
+        self.db['file_name_lower'] = self.db['file_name'].str.lower()
+
+
+    def search_query(self,query):
+        return self.db[self.db['file_name_lower'].str.contains(query)].iloc[:,:-1].sort_values(['Modified_time'],ascending=False)
+
+
+
+ 
+# if __name__ == "__main__":
+#     self = Filemanager(path = "C:/users/hp/downloads/")
+#     self.process()
+#     self.search_query(query = "nlp")
+        
